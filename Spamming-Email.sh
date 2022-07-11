7b168a21a01ea89492a4494f1bbf1515aec6c1d6
Syahdan Aziz <96916115+SyahdanAzizITNS@users.noreply.github.com>
2022-07-11 09:13:07 +0700

    Update no remove run.sh

--- Spamming-Email.sh
+++ Spamming-Email.sh
@@ -12,13 +12,13 @@ read -r spam
 
 for i in $ ( eval echo (1..$spam) )
 do
-echo "telnet $telnet 25 << EOF" >> run.sh
-echo "mail from:$from" >> run.sh
-echo "rcpt to:$to" >> run.sh
-echo "data" >> run.sh
-echo "$message" >> run.sh
-echo "." >> run.sh
-echo " " >> run.sh
+echo "telnet $telnet 25 << EOF" > run.sh
+echo "mail from:$from" > run.sh
+echo "rcpt to:$to" > run.sh
+echo "data" > run.sh
+echo "$message" > run.sh
+echo "." > run.sh
+echo " " > run.sh
 done
 
 bash run.sh
