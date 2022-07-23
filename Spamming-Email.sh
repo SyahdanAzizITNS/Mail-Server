469d2bc36fe7417e9f22c268a95c4c080aa74ab3
Syahdan Aziz <96916115+SyahdanAzizITNS@users.noreply.github.com>
2022-07-24 04:07:30 +0700

    Update Spamming-Email.sh

--- Spamming-Email.sh
+++ Spamming-Email.sh
@@ -5,20 +5,24 @@ echo -n "email from: "
 read -r from
 
 echo -n "receipt to: "
-read -r message
+read -r to
+
+echo -n "massage: "
+read -r massage
 
 echo -n "how many spam: "
 read -r spam
 
-for i in $ ( eval echo (1..$spam) )
+for i in $ ( eval echo {1..$spam} )
 do
-echo "telnet $telnet 25 << EOF" > run.sh
-echo "mail from:$from" > run.sh
-echo "rcpt to:$to" > run.sh
-echo "data" > run.sh
-echo "$message" > run.sh
-echo "." > run.sh
-echo " " > run.sh
+echo "telnet $telnet 25 << EOF" >> run.sh
+echo "mail from:$from" >> run.sh
+echo "rcpt to:$to" >> run.sh
+echo "data" >> run.sh
+echo "$message" >> run.sh
+echo "." >> run.sh
+echo " " >> run.sh
 done
 
 bash run.sh
+rm run.sh
